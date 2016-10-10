432a790e855a1d8a9ebefbfb7d3ff4254eca5d3c
Viacheslav Luzhanskyi <vluzhanskyi@gmail.com>
2016-10-10 18:21:29 +0300

    start

---
+++ Lesson15/Printer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15
+{
+   public class Printer
+    {
+        public void Talk() { Console.Write("Talk "); }
+        public void Is() { Console.Write("is "); }
+        public void Cheap() { Console.Write("cheap. "); }
+        public void Show() { Console.Write("Show "); }
+        public void me() { Console.Write("me "); }
+        public void the() { Console.Write("the "); }
+        public void code() { Console.Write("code "); }
+    }
+}
--- Lesson15/Program.cs
+++ Lesson15/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Lesson15
@@ -10,6 +12,18 @@ namespace Lesson15
     {
         static void Main(string[] args)
         {
+            Printer p = new Printer();
+
+            Thread thred1 = new Thread(p.Talk);
+            thred1.Start();
+            thred1.Abort();
+            Thread thred2 = new Thread(p.Is);
+            thred2.Start();
+            thred2.Abort();
+            Thread thred3 = new Thread(p.Cheap);
+            thred3.Start();
+            thred3.Abort();
+            Console.ReadKey();
         }
     }
 }
